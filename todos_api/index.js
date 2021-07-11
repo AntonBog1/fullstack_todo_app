9f42fcd2598470f5cda16c0bbf047d599ec65a4b
Anton <antonbogatirev1@gmail.com>
2021-07-10 18:52:03 -0700

    Running two servers
    Changed the todos_api port from 3000 to 3001 so we can run two servers at the same time. The api one and the react one.
    Created a TodoList component.

--- todos_api/index.js
+++ todos_api/index.js
@@ -14,6 +14,6 @@ app.get('/', function (req, res) {
 
 app.use('/api/todos', todoRoutes);
 
-app.listen(3000, function () {
-    console.log('App is live on port 3000')
+app.listen(3001, function () {
+    console.log('App is live on port 3001')
 });
---
+++ todosfrontend/src/TodoList.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class TodoList extends Component {
+    render(){
+        return (
+            <h1>Todo List!</h1>
+        )
+    }
+}
+
+export default TodoList;
