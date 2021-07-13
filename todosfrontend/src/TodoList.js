cbeb8a29d6f8a090c4f0f8770856da14a7911468
Anton <antonbogatirev1@gmail.com>
2021-07-13 16:02:02 -0700

    Adding todos form
    Added a TodoList component.
    Added addTodo function that sends a post request to the API and creates a new todo then updates the state to show the new todo.

--- todosfrontend/src/TodoList.js
+++ todosfrontend/src/TodoList.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import TodoItem from './TodoItem';
+import TodoForm from './TodoForm';
 const APIURL = '/api/todos';
 
 
@@ -9,6 +10,7 @@ class TodoList extends Component {
         this.state = {
             todos: []
         }
+        this.addTodo = this.addTodo.bind(this);
     }
 
     componentWillMount() {
@@ -34,6 +36,33 @@ class TodoList extends Component {
             .then(todos => this.setState({ todos }));
     }
 
+    addTodo(newTodoInput){
+        fetch(APIURL, {
+            method: 'post',
+            headers: new Headers({
+                'Content-Type': 'application/json'
+            }),
+            body: JSON.stringify({name: newTodoInput})
+        })
+            .then(resp => {
+                if (!resp.ok) {
+                    if (resp.status >= 400 && resp.status < 500) {
+                        return resp.json().then(data => {
+                            let err = { errorMessage: data.message };
+                            throw err;
+                        })
+                    } else {
+                        let err = { errorMessage: 'Please try again later, server is not responding.' };
+                        throw err;
+                    }
+                }
+                return resp.json();
+            })
+            .then(newTodo => {
+                this.setState({todos: [...this.state.todos, newTodo]})
+            })
+    };
+
 
     render() {
         const todos = this.state.todos.map((t) => (
@@ -45,6 +74,7 @@ class TodoList extends Component {
         return (
             <div>
                 <h1>Todo List!</h1>
+                <TodoForm addTodo={this.addTodo}/>
                 <ul>
                     {todos}
                 </ul>
