bec57a53249d6f3dd5b74fd02a1536b9b4be538b
Anton <antonbogatirev1@gmail.com>
2021-07-14 11:40:31 -0700

    Toggling todos
    Added a toggleTodo function that updates a todo to completed or not and sets a new state.

--- todosfrontend/src/TodoItem.js
+++ todosfrontend/src/TodoItem.js
@@ -1,12 +1,15 @@
 import React from 'react';
 
-const TodoItem = ({ name, completed, onDelete }) => (
-    <li
-        style={{
-            textDecoration: completed ? 'line-through' : 'none'
-        }}
-    >
-        {name}
+const TodoItem = ({ name, completed, onDelete, onToggle }) => (
+    <li>
+        <span
+            style={{
+                textDecoration: completed ? 'line-through' : 'none'
+            }}
+            onClick={onToggle}
+        >
+            {name}
+        </span>
         <span onClick={onDelete}> X </span>
     </li>
 );
--- todosfrontend/src/TodoList.js
+++ todosfrontend/src/TodoList.js
@@ -88,12 +88,46 @@ class TodoList extends Component {
             });
     }
 
+    toggleTodo(todo) {
+        const updateURL = APIURL + todo._id;
+        fetch(updateURL, {
+            method: 'put',
+            headers: new Headers({
+                'Content-Type': 'application/json'
+            }),
+            body: JSON.stringify({ completed: !todo.completed })
+        })
+            .then(resp => {
+                if (!resp.ok) {
+                    if (resp.status >= 400 && resp.status < 500) {
+                        return resp.json().then(data => {
+                            let err = { errorMessage: data.message };
+                            throw err;
+                        })
+                    } else {
+                        let err = { errorMessage: 'Please try again later, server is not responding.' };
+                        throw err;
+                    }
+                }
+                return resp.json();
+            })
+            .then(updatedTodo => {
+                const todos = this.state.todos.map(t =>
+                    (t._id === updatedTodo._id)
+                    ? {...t, completed: !t.completed}
+                    : t
+                    )
+                this.setState({ todos })
+            });
+    }
+
     render() {
         const todos = this.state.todos.map((t) => (
             <TodoItem
                 key={t._id}
                 {...t}
                 onDelete={this.deleteTodo.bind(this, t._id)}
+                onToggle={this.toggleTodo.bind(this, t)}
             />
         ));
         return (
